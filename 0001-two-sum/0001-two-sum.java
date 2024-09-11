bdae3b340b0518a1d802c641dde5222913d90c12
Viral Ahir <92668360+Viral-Ahir@users.noreply.github.com>
2024-09-11 10:28:57 -0400

    Time: 2 ms (98.48%), Space: 45 MB (19.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,15 @@
 class Solution {
+    int sum = 0;
+
     public int[] twoSum(int[] nums, int target) {
-        
+        Map<Integer, Integer> numberMap = new HashMap<>(); 
+        for (int i = 0; i<nums.length; i++){
+            int remaining = target - nums[i];
+            if (numberMap.containsKey(remaining)) {
+                return new int[]{numberMap.get(remaining), i};
+            }    
+            numberMap.put(nums[i],i);
+        }
+        return new int[]{};
     }
 }
