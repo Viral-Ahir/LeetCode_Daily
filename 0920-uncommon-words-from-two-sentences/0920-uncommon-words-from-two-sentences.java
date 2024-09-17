546b226ad7b21c4abd0a25cee4c20d556a0f59db
Viral Ahir <92668360+Viral-Ahir@users.noreply.github.com>
2024-09-17 07:47:20 -0400

    Time: 5 ms (20.24%), Space: 41.9 MB (69.28%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
@@ -1,34 +1,54 @@
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
-        List<String> uncommonList = new ArrayList<>();
-        List<String> viewed = new ArrayList<>();
+       String s = s1 + " " + s2;
 
-        String[] str = s1.split(" ");
-        System.out.println(Arrays.toString(str));
-        for(String word: str){
-            if (viewed.contains(word)){
-                continue;
-            }
-            if(uncommonList.contains(word)){
-                uncommonList.remove(word);
-                viewed.add(word);
-            } else {
-                uncommonList.add(word);
-            }
+       Map<String, Integer> wordCount = new HashMap<>();
+
+       String[] words = s.split(" ");
+
+       for (String word: words){
+            wordCount.put(word, wordCount.getOrDefault(word, 0) +1);
+       }
+
+       List<String> result = new ArrayList<>();
+
+       for(Map.Entry<String, Integer> entry: wordCount.entrySet()){
+        if(entry.getValue() == 1){
+            result.add(entry.getKey());
         }
+       } 
+       return result.toArray(new String[0]); 
+   
+    // MORE SPACE    
+    //     List<String> uncommonList = new ArrayList<>();
+    //     List<String> viewed = new ArrayList<>();
+
+    //     String[] str = s1.split(" ");
+    //     System.out.println(Arrays.toString(str));
+    //     for(String word: str){
+    //         if (viewed.contains(word)){
+    //             continue;
+    //         }
+    //         if(uncommonList.contains(word)){
+    //             uncommonList.remove(word);
+    //             viewed.add(word);
+    //         } else {
+    //             uncommonList.add(word);
+    //         }
+    //     }
         
-        String[] str1 = s2.split(" ");
-        for (String word: str1){
-            if (viewed.contains(word)){
-                continue;
-            }
-            if(uncommonList.contains(word)){
-                uncommonList.remove(word);
-                viewed.add(word);
-            } else {
-                uncommonList.add(word);
-            }
-        }
-        return uncommonList.toArray(new String[0]);
+    //     String[] str1 = s2.split(" ");
+    //     for (String word: str1){
+    //         if (viewed.contains(word)){
+    //             continue;
+    //         }
+    //         if(uncommonList.contains(word)){
+    //             uncommonList.remove(word);
+    //             viewed.add(word);
+    //         } else {
+    //             uncommonList.add(word);
+    //         }
+    //     }
+    //     return uncommonList.toArray(new String[0]);
     }
 }
