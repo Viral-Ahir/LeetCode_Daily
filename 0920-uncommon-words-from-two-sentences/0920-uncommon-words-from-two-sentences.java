2181e172af70ab57daac6b1800b6fd340c35e4a5
Viral Ahir <92668360+Viral-Ahir@users.noreply.github.com>
2024-09-17 07:48:04 -0400

    Time: 5 ms (20.24%), Space: 42.2 MB (22.88%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
@@ -18,37 +18,5 @@ class Solution {
         }
        } 
        return result.toArray(new String[0]); 
-   
-    // MORE SPACE    
-    //     List<String> uncommonList = new ArrayList<>();
-    //     List<String> viewed = new ArrayList<>();
-
-    //     String[] str = s1.split(" ");
-    //     System.out.println(Arrays.toString(str));
-    //     for(String word: str){
-    //         if (viewed.contains(word)){
-    //             continue;
-    //         }
-    //         if(uncommonList.contains(word)){
-    //             uncommonList.remove(word);
-    //             viewed.add(word);
-    //         } else {
-    //             uncommonList.add(word);
-    //         }
-    //     }
-        
-    //     String[] str1 = s2.split(" ");
-    //     for (String word: str1){
-    //         if (viewed.contains(word)){
-    //             continue;
-    //         }
-    //         if(uncommonList.contains(word)){
-    //             uncommonList.remove(word);
-    //             viewed.add(word);
-    //         } else {
-    //             uncommonList.add(word);
-    //         }
-    //     }
-    //     return uncommonList.toArray(new String[0]);
     }
 }
