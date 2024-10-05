769a0446d6e19780d91fbaf38950d22b5b522f9b
Viral Ahir <92668360+Viral-Ahir@users.noreply.github.com>
2024-10-05 14:46:04 -0400

    Time: 12 ms (51.37%), Space: 57.8 MB (56.95%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,16 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // Create a hash map
         Set<Integer> hs = new HashSet<>();
-
         for (int i: nums){
             if (hs.contains(i)){
                 return true;
             }
             hs.add(i);
         }
-
         return false;
-        
     }
 }
